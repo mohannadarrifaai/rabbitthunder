3792899b6096c671da1843287f321053bdee504c
mohannadarrifaai <91025721+mohannadarrifaai@users.noreply.github.com>
2024-08-04 20:10:26 +0200

    Update test.ts

--- api/test.ts
+++ api/test.ts
@@ -108,5 +108,5 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   );
   console.log(finalResponse);
-  res.json(finalResponse);
+  res.json(logger);
 };
