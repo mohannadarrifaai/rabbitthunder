ae8dcef2ee5d168368371cc59dcd4eeffbcdb6b2
mohannadarrifaai <91025721+mohannadarrifaai@users.noreply.github.com>
2024-08-12 22:31:07 +0200

    Update click.ts

--- api/click.ts
+++ api/click.ts
@@ -83,7 +83,7 @@ export default async (req, res) => {
     await page.goto(url);
     await page.waitForSelector("#btn-play", { timeout: 5_000 });
     try {
-      //for (let i = 0; i < 50; i++) {
+      for (let i = 0; i < 50; i++) {
         //if (closed) {
           //break;
         //}
