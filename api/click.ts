c5fc6dd98c79260b13471f3214a21d1f500b0c44
mohannadarrifaai <91025721+mohannadarrifaai@users.noreply.github.com>
2024-08-13 03:47:56 +0200

    Update click.ts

--- api/click.ts
+++ api/click.ts
@@ -1,6 +1,26 @@
 const puppeteer = require('puppeteer-extra');
 const chrome = require('@sparticuz/chromium');
 
+require('puppeteer-extra-plugin-user-data-dir');
+require('puppeteer-extra-plugin-user-preferences');
+require('puppeteer-extra-plugin-stealth/evasions/chrome.app');
+require('puppeteer-extra-plugin-stealth/evasions/chrome.csi');
+require('puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes');
+require('puppeteer-extra-plugin-stealth/evasions/chrome.runtime');
+require('puppeteer-extra-plugin-stealth/evasions/defaultArgs'); // pkg warned me this one was missing
+require('puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow');
+require('puppeteer-extra-plugin-stealth/evasions/media.codecs');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.languages');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.permissions');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.plugins');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.vendor');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.webdriver');
+require('puppeteer-extra-plugin-stealth/evasions/sourceurl');
+require('puppeteer-extra-plugin-stealth/evasions/user-agent-override');
+require('puppeteer-extra-plugin-stealth/evasions/webgl.vendor');
+require('puppeteer-extra-plugin-stealth/evasions/window.outerdimensions');
+
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
@@ -26,11 +46,11 @@ export default async (req, res) => {
   const url = body.url;
   const referer = body.referer;
   const sleep = ms => new Promise(r => setTimeout(r, ms));
-    const args = [
+  const args = [
     '--no-sandbox',
     '--disable-web-security',
   ];
-    const options = {
+  const options = {
     args,
     executablePath: await chrome.executablePath(),
     headless: true,
@@ -47,18 +67,10 @@ export default async (req, res) => {
   })
   // Set headers, else wont work.
   await page.setExtraHTTPHeaders({ 'Referer': referer});
-
-  await page.setRequestInterception(true);
   const logger = [];
   const finalResponse = { source: []};
   page.on('request', async (request) => {
-    logger.push(request.url());
-    if (request.url().includes('.m3u8')) finalResponse.source.push(request.url());
-    // console.log(request.url());
-      request.continue();
-  });
-  // return new Promise(async (resolve) => {
-return new Promise(async (resolve) => {
+	return new Promise(async (resolve) => {
     await page.goto(url, { waitUntil: 'networkidle0' });
     await page.waitForSelector("#btn-play", { timeout: 50000 });
     try {
@@ -70,16 +82,19 @@ return new Promise(async (resolve) => {
       }
         await sleep(10000);
     }
-    }
-    catch (e) {
-       return res.status(500).end(`${e}`);
-    }
-    resolve(keys);
-  });
     if (browser) {
       await sleep(2500);
       await browser.close();
     }
+    resolve(keys);	
+    }
+    catch (e) {
+       return res.status(500).end(`${e}`);
+    }
+
+	
+  });
+
 
   // Response headers.
   res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
