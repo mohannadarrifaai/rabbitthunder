7bfbc9ba997adf442e38e7e53481ccca9fb054f7
mohannadarrifaai <91025721+mohannadarrifaai@users.noreply.github.com>
2024-08-15 21:18:25 +0200

    Update test2.js

--- api/test2.js
+++ api/test2.js
@@ -1,9 +1,126 @@
-import express from 'express';
+const puppeteer = require('puppeteer-extra');
+const chrome = require('@sparticuz/chromium');
 
-const app = express();
+// Stealth plugin issue - There is a good fix but currently this works.
+require('puppeteer-extra-plugin-user-data-dir');
+require('puppeteer-extra-plugin-user-preferences');
+require('puppeteer-extra-plugin-stealth/evasions/chrome.app');
+require('puppeteer-extra-plugin-stealth/evasions/chrome.csi');
+require('puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes');
+require('puppeteer-extra-plugin-stealth/evasions/chrome.runtime');
+require('puppeteer-extra-plugin-stealth/evasions/defaultArgs'); // pkg warned me this one was missing
+require('puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow');
+require('puppeteer-extra-plugin-stealth/evasions/media.codecs');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.languages');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.permissions');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.plugins');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.vendor');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.webdriver');
+require('puppeteer-extra-plugin-stealth/evasions/sourceurl');
+require('puppeteer-extra-plugin-stealth/evasions/user-agent-override');
+require('puppeteer-extra-plugin-stealth/evasions/webgl.vendor');
+require('puppeteer-extra-plugin-stealth/evasions/window.outerdimensions');
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
 
-export default app;
+export default async (req, res) => {
+  let { body, method } = req;
+
+  // Some header shits
+  if (method !== 'POST') {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+    return res.status(200).end();
+  }
+
+  // Some checks...
+  if (!body) return res.status(400).end(`No body provided`);
+  if (typeof body === 'object' && !body.id) return res.status(400).end(`No url provided`);
+
+  const id = body.id;
+  const isProd = process.env.NODE_ENV === 'production';
+
+  // create browser based on ENV
+  let browser;
+  if (isProd) {
+    browser = await puppeteer.launch({
+      args: chrome.args,
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath(),
+      headless: true,
+      ignoreHTTPSErrors: true
+    });
+  } else {
+    browser = await puppeteer.launch({
+      headless: true,
+      executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    });
+  }
+  let keys = {};
+  const page = await browser.newPage();
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0');
+  await page.setViewport({
+    width: 1080,
+    height: 1080
+  })
+  // Set headers, else wont work.
+  await page.setExtraHTTPHeaders({ 'Referer': 'https://vidsrc.xyz/'});
+
+  await page.setRequestInterception(true);
+  const logger = [];
+  const finalResponse = { source: []};
+  page.on('request', async (request) => {
+    logger.push(request.url());
+    if (request.url().includes('.m3u8')) finalResponse.source.push(request.url());
+    console.log(request.url());
+      request.continue();
+  });
+  return new Promise(async (resolve) => {
+
+    await page.goto("https://vidsrc.xyz/embed/tv?imdb=tt1190634&season=1&episode=1", { waitUntil: 'networkidle0' });
+    // await page.waitForSelector("#pl_but",  { visible: true });
+    try {
+      for (let i = 0; i < 50; i++) {
+        // await page.bringToFront();
+        // let btn = await page.$("#pl_but");
+        // if (btn) {
+          const viewport = await page.viewport();
+          await page.mouse.click(viewport.width / 2, viewport.height / 2);
+      // }
+        await sleep(1000);
+    }
+    }
+    catch (e) {
+        console.log(`[x] ${e}`);
+    }
+
+    if (browser) {
+      await sleep(config.MAX_TIMEOUT);
+      await browser.close();
+    }
+    resolve(keys);
+  });
+
+}
+
+  // Response headers.
+  res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+  res.setHeader('Content-Type', 'application/json');
+  // CORS
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  console.log(finalResponse);
+  res.json(logger);
+};
