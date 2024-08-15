9be6b0be4697d10284e664c8715313c8e4d65834
mohannadarrifaai <91025721+mohannadarrifaai@users.noreply.github.com>
2024-08-15 23:55:25 +0200

    Update test2.ts

--- api/test2.ts
+++ api/test2.ts
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer-extra');
-const {executablePath} = require('puppeteer')
+//const {executablePath} = require('puppeteer')
 const chrome = require('@sparticuz/chromium');
 
 // Stealth plugin issue - There is a good fix but currently this works.
@@ -58,8 +58,11 @@ export default async (req, res) => {
     '--disable-web-security',
   ],
       defaultViewport: chrome.defaultViewport,
-      executablePath:process.env.PUPPETEER_EXEC_PATH,
-      //(puppeteer as any).executablePath();
+      //executablePath:process.env.PUPPETEER_EXEC_PATH,
+      //executablePath:(puppeteer as any).executablePath();
+      // executablePath:executablePath(),
+      executablePath:require('puppeteer').executablePath()
+      //executablePath:require('puppeteer-extra-plugin-stealth').executablePath()
       headless: true,
       ignoreHTTPSErrors: true
     });
