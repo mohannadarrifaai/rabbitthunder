96c6838ec7d33f2e91f91e07ff2ec3333705cba8
mohannadarrifaai <91025721+mohannadarrifaai@users.noreply.github.com>
2024-09-07 17:23:13 +0200

    Update click.ts

--- api/click.ts
+++ api/click.ts
@@ -83,10 +83,7 @@ export default async (req: any, res: any) => {
   });
   
   try {
-    const [req] = await Promise.all([
-      page.waitForRequest(req => req.url().includes('.m3u8'), { timeout: 20000 }),
-      page.goto(url, { waitUntil: 'domcontentloaded' }),
-    ]);
+      page.goto(url, { waitUntil: 'load' })
   } catch (error) {
     return res.status(500).end(`Server Error,check the params.`)
   }
