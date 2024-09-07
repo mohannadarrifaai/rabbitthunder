6cca867864129789a182279f6308e9111d061547
mohannadarrifaai <91025721+mohannadarrifaai@users.noreply.github.com>
2024-09-07 17:15:38 +0200

    Update click.ts

--- api/click.ts
+++ api/click.ts
@@ -1,109 +1,109 @@
+// written by cool-dev-guy
 const puppeteer = require('puppeteer-extra');
 const chrome = require('@sparticuz/chromium');
 
-require('puppeteer-extra-plugin-user-data-dir');
-require('puppeteer-extra-plugin-user-preferences');
-require('puppeteer-extra-plugin-stealth/evasions/chrome.app');
-require('puppeteer-extra-plugin-stealth/evasions/chrome.csi');
-require('puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes');
-require('puppeteer-extra-plugin-stealth/evasions/chrome.runtime');
-require('puppeteer-extra-plugin-stealth/evasions/defaultArgs'); // pkg warned me this one was missing
-require('puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow');
-require('puppeteer-extra-plugin-stealth/evasions/media.codecs');
-require('puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency');
-require('puppeteer-extra-plugin-stealth/evasions/navigator.languages');
-require('puppeteer-extra-plugin-stealth/evasions/navigator.permissions');
-require('puppeteer-extra-plugin-stealth/evasions/navigator.plugins');
-require('puppeteer-extra-plugin-stealth/evasions/navigator.vendor');
-require('puppeteer-extra-plugin-stealth/evasions/navigator.webdriver');
-require('puppeteer-extra-plugin-stealth/evasions/sourceurl');
-require('puppeteer-extra-plugin-stealth/evasions/user-agent-override');
-require('puppeteer-extra-plugin-stealth/evasions/webgl.vendor');
-require('puppeteer-extra-plugin-stealth/evasions/window.outerdimensions');
+// Stealth plugin issue - There is a good fix but currently this works.
+require('puppeteer-extra-plugin-user-data-dir')
+require('puppeteer-extra-plugin-user-preferences')
+require('puppeteer-extra-plugin-stealth/evasions/chrome.app')
+require('puppeteer-extra-plugin-stealth/evasions/chrome.csi')
+require('puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes')
+require('puppeteer-extra-plugin-stealth/evasions/chrome.runtime')
+require('puppeteer-extra-plugin-stealth/evasions/defaultArgs') // pkg warned me this one was missing
+require('puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow')
+require('puppeteer-extra-plugin-stealth/evasions/media.codecs')
+require('puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency')
+require('puppeteer-extra-plugin-stealth/evasions/navigator.languages')
+require('puppeteer-extra-plugin-stealth/evasions/navigator.permissions')
+require('puppeteer-extra-plugin-stealth/evasions/navigator.plugins')
+require('puppeteer-extra-plugin-stealth/evasions/navigator.vendor')
+require('puppeteer-extra-plugin-stealth/evasions/navigator.webdriver')
+require('puppeteer-extra-plugin-stealth/evasions/sourceurl')
+require('puppeteer-extra-plugin-stealth/evasions/user-agent-override')
+require('puppeteer-extra-plugin-stealth/evasions/webgl.vendor')
+require('puppeteer-extra-plugin-stealth/evasions/window.outerdimensions')
 
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-puppeteer.use(StealthPlugin());
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
-export default async (req, res) => {
-  let { body, method } = req;
+export default async (req: any, res: any) => {
+  let {body,method} = req
 
   // Some header shits
   if (method !== 'POST') {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
     res.setHeader(
       'Access-Control-Allow-Headers',
       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-    return res.status(200).end();
+    )
+    return res.status(200).end()
   }
 
   // Some checks...
-  if (!body) return res.status(400).end(`No body provided`);
-  if (typeof body === 'object' && !body.url) return res.status(400).end(`No url provided`);
-
+  if (!body) return res.status(400).end(`No body provided`)
+  if (typeof body === 'object' && !body.url) return res.status(400).end(`No url provided`)
+  
   const url = body.url;
-  const referer = body.referer;
-  const sleep = ms => new Promise(r => setTimeout(r, ms));
-	let browser
+  const isProd = process.env.NODE_ENV === 'production'
+
+  // create browser based on ENV
+  let browser;
+  if (isProd) {
     browser = await puppeteer.launch({
       args: chrome.args,
       defaultViewport: chrome.defaultViewport,
       executablePath: await chrome.executablePath(),
       headless: true,
       ignoreHTTPSErrors: true
-	       });
+    })
+  } else {
+    browser = await puppeteer.launch({
+      headless: true,
+      executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    })
+  }
   const page = await browser.newPage();
-  let keys = {};
   await page.setRequestInterception(true);
-  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36; PlayStation');
-  await page.setViewport({
-    width: 1080,
-    height: 1080
-  })
-  // Set headers, else wont work.
-  await page.setExtraHTTPHeaders({ 'Referer': referer});
-  const logger = [];
-  const finalResponse = { source: []};
-  page.on('request', async (request) => {
-	return new Promise(async (resolve) => {
-    await page.goto(url, { waitUntil: 'networkidle0' });
-    await page.waitForSelector("#player > div > div > div.css-f61ful > button:nth-child(2) > svg > path", { timeout: 20000 });
-    try {
-      for (let i = 0; i < 5; i++) {
-        await page.bringToFront();
-        let btn = await page.$("#player > div > div > div.css-f61ful > button:nth-child(2) > svg > path");
-        if (btn) {
-          btn.click();
-      }
-        await sleep(10000);
-    }
-    if (browser) {
-      await sleep(2500);
-      await browser.close();
-    }
-    resolve(keys);	
-    }
-    catch (e) {
-       return res.status(500).end(`${e}`);
-    }
 
-	
+  // Set headers,else wont work.
+  await page.setExtraHTTPHeaders({ 'Referer': 'https://vidsrc.pro/' });
+  
+  const logger:string[] = [];
+  const finalResponse:{source:string,subtitle:string[]} = {source:'',subtitle:[]}
+  
+  page.on('request', async (interceptedRequest) => {
+    await (async () => {
+      logger.push(interceptedRequest.url());
+      if (interceptedRequest.url().includes('.m3u8')) finalResponse.source = interceptedRequest.url();
+      if (interceptedRequest.url().includes('.vtt')) finalResponse.subtitle.push(interceptedRequest.url());
+      interceptedRequest.continue();
+    })();
   });
-
+  
+  try {
+    const [req] = await Promise.all([
+      page.waitForRequest(req => req.url().includes('.m3u8'), { timeout: 20000 }),
+      page.goto(url, { waitUntil: 'domcontentloaded' }),
+    ]);
+  } catch (error) {
+    return res.status(500).end(`Server Error,check the params.`)
+  }
+  await browser.close();
 
   // Response headers.
-  res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalurlate')
+  res.setHeader('Content-Type', 'application/json')
   // CORS
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  // res.setHeader('Access-Control-Allow-Headers', '*')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
+  )
   console.log(finalResponse);
-  res.json(logger);
+  res.json(finalResponse);
 };
